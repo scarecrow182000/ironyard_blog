fa617c44e36909837d95028329ccd3e524e85382
Eric Epperson <eric_d_epperson@hotmail.com>
2014-06-26 14:26:45 -0500

    Blog Posts

--- config.rb
+++ config.rb
@@ -34,7 +34,7 @@ end
 # Required
 set :blog_url, 'http://www.example.com'
 set :blog_name, 'Eric Epperson'
-set :blog_description, 'Makes developing websites simple.'
+set :blog_description, 'My IronYard Academy Experience'
 set :author_name, 'Middleman'
 set :author_bio, 'Middleman is a static site generator using all the ' \
                  'shortcuts and tools in modern web development.'
